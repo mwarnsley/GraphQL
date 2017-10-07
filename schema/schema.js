060e9abe829c7927a073d225b329f2b9a27ca493
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-10-07 16:30:53 -0400

    Adding in the resolve function for returning the company associated with the user

--- schema/schema.js
+++ schema/schema.js
@@ -33,6 +33,9 @@ const UserType = new GraphQLObjectType({
     age: {type: GraphQLInt},
     company: {
       type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then(res => res.data);
+      },
     },
   },
 });
