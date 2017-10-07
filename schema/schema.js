54741277e93f971d59fbf821599a129107cbc439
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-10-07 14:50:25 -0400

    Setting up the user type object for the user schema for graphql

--- schema/schema.js
+++ schema/schema.js
@@ -3,3 +3,22 @@
  * What properties each object has
  * Exactly how each property is related to another
  */
+
+// Importing the modules needed for the schema file
+const graphql = require('graphql');
+const {GraphQLObjectType, GraphQLString, GraphQLInt} = graphql;
+
+/*
+ * Object instructs graphql what the user object looks like
+ * name is a required property
+ * fields is a required property as an object with id, firstName, age
+ * fields properties are objects with a type property using built in types from GraphQL
+ */
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {type: GraphQLString},
+    firstName: {type: GraphQLString},
+    age: {type: GraphQLInt},
+  },
+});
