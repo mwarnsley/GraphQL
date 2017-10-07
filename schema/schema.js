072a9c2bb0ff9258df121ef1ed7d4104ab8522c0
Marcus Warnsley <marcus@PC0159-Jscott.local>
2017-10-07 15:13:58 -0400

    Setting up the root query for graphql

--- schema/schema.js
+++ schema/schema.js
@@ -22,3 +22,23 @@ const UserType = new GraphQLObjectType({
     age: {type: GraphQLInt},
   },
 });
+
+/*
+ * Root query provides the entry point into our data
+ * name is a required property
+ * fields is a required property as an object with user
+ * user is an object with properties of type and args
+ * type used the UserType const
+ * args is an object with and id with type of GraphQLString
+ * resolve function tries to go into the database and find actual data
+ */
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve(parentValue, args) {},
+    },
+  },
+});
